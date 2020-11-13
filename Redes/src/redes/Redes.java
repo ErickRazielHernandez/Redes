b07abb28752933c4bcc9ceea32877551e225c394
ErickRazielHernandez <44034832+ErickRazielHernandez@users.noreply.github.com>
2020-11-13 12:21:44 -0600

    Add files via upload

---
+++ Redes/src/redes/Redes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template file, choose Tools | Templates | Open the template in the editor.
+ */
+package redes;
+
+/**
+ *
+ * @author HP
+ */
+public class Redes {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
